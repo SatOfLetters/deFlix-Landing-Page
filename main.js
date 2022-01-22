d3314a78d0fbe86fe9d534bdb8742888eb7fc38e
SatOfLetters <97686395+SatOfLetters@users.noreply.github.com>
2022-01-22 19:32:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-ScrollReveal().reveal('.text_ctnr p, .bullets, .chair_img, .inefficiency, .megaphone', {
+ScrollReveal().reveal('.text_ctnr p, .bullets, .chair_img, .inefficiency', {
     duration: 800,
     easing: 'ease'
 });
+
+
+
+const nav = document.querySelector('header nav')
+const hamBt = document.querySelector('.hambt')
+const hambFill = document.querySelector('.hambt div div')
+
+let showingMenu = false;
+
+
+function hamburguer() {
+    if (showingMenu == false) {
+        nav.style.transform = 'translateY(0)';
+        hambFill.style.transform = 'translateY(0)'
+
+        showingMenu = true;
+    } else {
+        nav.style.transform = 'translateY(100%)';
+        hambFill.style.transform = 'translateY(100%)'
+
+        showingMenu = false;
+    }
+}
+
+hamBt.addEventListener('click', hamburguer)
+
+document.body.onscroll = function () {
+    if (showingMenu == true) {
+        hamburguer()
+    }
+}
